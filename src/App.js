c7fb8a1837ac1619d709f378a7fa436fa8c0bdf2
Shreya Khandal <shreyakhandal@gmail.com>
2022-01-26 15:53:03 +0530

    Create React app and publishing

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CatalogueProject from "./components/CatalogueProject";
+
+function App() {
+  return (
+    <div>
+      <CatalogueProject />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FilterDropdown.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+
+function FilterDropdown({posts, setCatalogue}) {
+  const [active, setActive] = useState(false)
+  
+  function filterTaskCompleted() {
+      return (posts.filter(post => post.completed === true))
+  }
+  function filterTaskIncompleted() {
+      return (
+          posts.filter(post => post.completed === false)
+          
+          )
+  }
+  function filterUserId1() {
+    return (posts.filter(post => post.userId === 1))
+}
+  return (
+    <div className='dropdown'>
+     <div className="dropdown-btn" onClick={() => setActive(!active)}>Choose any filter<i className="bi bi-caret-down-fill"></i></div>
+     {active &&
+       <div className='dropdown-content'>
+        <div className="dropdown-item" onClick={() => setCatalogue(posts)}>No Filter</div>
+        <div className="dropdown-item" onClick={() => setCatalogue(filterTaskCompleted())}>IDs with Task Completed</div>
+        <div className="dropdown-item" onClick={() => setCatalogue(filterTaskIncompleted())}>IDs with Task Incompleted</div>
+        <div className="dropdown-item" onClick={() => setCatalogue(filterUserId1())}>IDs with UserId1</div>
+    </div> 
+    }
+         
+  </div>
+  )
+}
+
+export default FilterDropdown;
